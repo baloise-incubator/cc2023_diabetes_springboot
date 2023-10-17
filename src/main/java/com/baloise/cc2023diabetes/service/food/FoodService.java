fc43de0eb20606278217c25fda604ae478d9d75c
Jan Moritz Hoefner <JanMoritz.Hoefner@baloise.ch>
2023-10-17 16:33:01 +0200

    Add CarbohydrateUnits to food api.

--- src/main/java/com/baloise/cc2023diabetes/service/food/FoodService.java
+++ src/main/java/com/baloise/cc2023diabetes/service/food/FoodService.java
@@ -13,14 +13,16 @@ import static com.baloise.cc2023.diabetes.jooq.Tables.FOOD;
 @AllArgsConstructor
 public class FoodService {
 
-	private final DSLContext jooq;
+    private final DSLContext jooq;
 
-	public List<FoodModel> all() {
-		return jooq.select(FOOD.ID, FOOD.TITLE, FOOD.UNIT, FOOD.CARBOHYDRATE_AVAILABLE, FOOD.SUGAR).from(FOOD).fetchInto(FoodModel.class);
-	}
+    public List<FoodModel> all() {
+        return jooq.select(FOOD.ID, FOOD.TITLE, FOOD.UNIT, FOOD.CARBOHYDRATE_AVAILABLE,
+                FOOD.CARBOHYDRATE_AVAILABLE.div(10.0), FOOD.SUGAR).from(FOOD).fetchInto(FoodModel.class);
+    }
 
-	public List<FoodModel> search(String searchString) {
-		return jooq.select(FOOD.ID, FOOD.TITLE, FOOD.UNIT, FOOD.CARBOHYDRATE_AVAILABLE, FOOD.SUGAR).from(FOOD)
-				.where(FOOD.TITLE.like("%" + searchString + "%")).fetchInto(FoodModel.class);
-	}
+    public List<FoodModel> search(String searchString) {
+        return jooq.select(FOOD.ID, FOOD.TITLE, FOOD.UNIT, FOOD.CARBOHYDRATE_AVAILABLE,
+                        FOOD.CARBOHYDRATE_AVAILABLE.div(10.0), FOOD.SUGAR).from(FOOD)
+                .where(FOOD.TITLE.like("%" + searchString + "%")).fetchInto(FoodModel.class);
+    }
 }
--- src/main/java/com/baloise/cc2023diabetes/service/food/model/FoodModel.java
+++ src/main/java/com/baloise/cc2023diabetes/service/food/model/FoodModel.java
@@ -1,3 +1,5 @@
 package com.baloise.cc2023diabetes.service.food.model;
 
-public record FoodModel(long id, String title, String unit, Double carbohydrateAvailable, Double sugar) { }
+public record FoodModel(long id, String title, String unit, Double carbohydrateAvailable, Double carbohydrateUnits,
+                        Double sugar) {
+}
