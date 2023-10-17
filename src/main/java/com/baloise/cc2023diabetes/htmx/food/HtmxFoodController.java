d8b8e18df54801d8b36cfe225c58cd5dc4626850
Sven Ehrke <sven.ehrke@sven-ehrke.de>
2023-10-17 18:36:45 +0200

    selection of food-items working

--- src/main/java/com/baloise/cc2023diabetes/htmx/food/HtmxFoodController.java
+++ src/main/java/com/baloise/cc2023diabetes/htmx/food/HtmxFoodController.java
@@ -3,9 +3,12 @@ package com.baloise.cc2023diabetes.htmx.food;
 import com.baloise.cc2023diabetes.service.food.FoodService;
 import com.baloise.cc2023diabetes.service.food.model.FoodModel;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,6 +17,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @Controller
@@ -47,17 +51,28 @@ public class HtmxFoodController {
     }
 
     @PostMapping(path = "/saveSelectedFood", consumes = {MediaType.APPLICATION_FORM_URLENCODED_VALUE})
-    public String saveSelectedFood(SaveFoodPayload payload, Model model) {
+    public void saveSelectedFood(SaveFoodPayload payload, Model model, HttpServletResponse response) {
         selectedFood.getTitles().add(payload.title());
         model.addAttribute("message", payload.title() + " put into session");
-        return "food/play";
+        response.setHeader("HX-Trigger", "selectedFoodSaved");
+        response.setStatus(HttpStatus.CREATED.value());
+//        return "food/play";
     }
 
     @GetMapping("/getSession")
-    public String getSession(HttpSession session, Model model) {
+    public String getSession(Model model) {
         String title = selectedFood.getTitles().isEmpty() ? "-" : selectedFood.getTitles().get(0);
         model.addAttribute("message", title + " read from session");
         return "food/play";
+    }
+
+
+    @GetMapping("/chips")
+    public String chips(Model model) {
+        String title = selectedFood.getTitles().isEmpty() ? "-" : selectedFood.getTitles().get(0);
+        model.addAttribute("items", selectedFood.getTitles());
+//        model.addAttribute("items", Arrays.asList("1", "2", "3"));
+        return "food/chips";
     }
 
 
